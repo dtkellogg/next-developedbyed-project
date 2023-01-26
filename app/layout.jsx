7ff12c3f5308700b9a2d7c0cc40a4013f1b2221b
Toshi <dtkellogg10@gmail.com>
2023-01-26 17:33:27 -0400

    Fonts - imported google font using Next 13 feature

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Montserrat } from '@next/font/google'  // you can import any font from google very easily
+
+const montserrat =  Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: '--font-montserrat'  // to use this with tailwind && add it in tailwind.config.js
+})
 
 export default function RootLayout({ children }) {
   return (
@@ -8,9 +15,9 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={`${montserrat.className}`}>
         <nav>
-          <ul>
+          <ul className="font-montserrat">
             <li>Home</li>
             <li>About</li>
           </ul>
@@ -20,3 +27,5 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+// the ul class font-montserrat isn't working... not sure why
